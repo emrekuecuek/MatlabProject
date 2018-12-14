0be8f95de9b67ad0f0f9cbc03e711e055c88ea51
Emre Kucuk <kucukemre96@gmail.com>
2018-12-14 04:12:27 +0300

    graphic-adjustment-needed

---
+++ julia.m
@@ -0,0 +1,12 @@
+function M = julia(zMax, c, N)
+    realPart = linspace(-zMax,zMax,500);
+    imaginaryPart = linspace(-zMax*1i,zMax*1i,500);
+    [X, Y] = meshgrid(realPart,imaginaryPart); 
+    Z = X + Y;
+    M = [];
+    for k = 1:500
+        for j = 1:500
+           M(k,j) = escapeVelocity(Z(j,k), c, N);
+        end
+    end
+end
